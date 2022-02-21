0c638e80e71a1776d2536433b704f0e7f2e002c2
Saleh Zaidan <salehzaidan64@gmail.com>
2022-02-21 21:22:42 +0700

    Add InterVariable font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
