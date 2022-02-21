0058f5c03be682615142575c0e0a70ef120825ae
Saleh Zaidan <salehzaidan64@gmail.com>
2022-02-21 20:44:25 +0700

    Remove boilerplate and configure prettier

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Stasiun Pengisian Kendaraan Listrik ITB</title>
+        <meta
+          name="description"
+          content="Stasiun Pengisian Kendaraan Listrik ITB"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>SPKL ITB</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
