db2ba7c4bfaf519046379250e2bea5388652cc01
Saleh Zaidan <salehzaidan64@gmail.com>
2022-02-22 10:40:03 +0700

    Separate daily data to raw and display

--- src/lib/daily.ts
+++ src/lib/daily.ts
@@ -1,3 +1,6 @@
+import currency from 'currency.js';
+import { format, formatDuration, intervalToDuration } from 'date-fns';
+
 export interface DailyRawData {
   transaction_pk: number;
   start_timestamp: string;
@@ -6,5 +9,59 @@ export interface DailyRawData {
   stop_value: number;
 }
 
+export interface DailyData {
+  transactionId: number;
+  start: Date;
+  stop: Date;
+  chargingTime: Duration;
+  energy: number;
+  cost: currency;
+}
+
+export interface DailyDisplayData {
+  transactionId: number;
+  start: string;
+  stop: string;
+  chargingTime: string;
+  energy: number;
+  cost: string;
+}
+
 export const dateRawFormat = 'yyyy-MM-dd';
 export const dateDisplayFormat = 'dd MMMM yyyy';
+export const timestampDisplayFormat = 'dd MMMM yyyy, HH:mm:ss';
+
+const costFactor = 2.466;
+const currencyOptions = {
+  symbol: 'Rp',
+  separator: '.',
+  decimal: ',',
+  precision: 0,
+};
+
+export function parseDailyData(rawData: DailyRawData[]): DailyData[] {
+  return rawData.map(d => {
+    const start = new Date(d.start_timestamp);
+    const stop = new Date(d.stop_timestamp);
+    const energy = d.stop_value - d.start_value;
+
+    return {
+      transactionId: d.transaction_pk,
+      start,
+      stop,
+      chargingTime: intervalToDuration({ start, end: stop }),
+      energy,
+      cost: currency(energy * costFactor, { ...currencyOptions, symbol: '' }),
+    };
+  });
+}
+
+export function displayDailyData(data: DailyData[]): DailyDisplayData[] {
+  return data.map(d => ({
+    ...d,
+    start: format(d.start, timestampDisplayFormat),
+    stop: format(d.stop, timestampDisplayFormat),
+    chargingTime: formatDuration(d.chargingTime),
+    cost: d.cost.format(),
+  }));
+}
--- src/pages/daily.tsx
+++ src/pages/daily.tsx
@@ -1,30 +1,48 @@
 import type { NextPage } from 'next';
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import DatePicker from 'react-datepicker';
 import CustomDateInput from '../components/CustomDateInput';
 import Layout from '../components/Layout';
 import Table from '../components/Table';
-import type { DailyRawData } from '../lib/daily';
-import { dateDisplayFormat, dateRawFormat } from '../lib/daily';
+import type { DailyRawData, DailyData } from '../lib/daily';
+import {
+  dateDisplayFormat,
+  dateRawFormat,
+  parseDailyData,
+  displayDailyData,
+} from '../lib/daily';
 import useFilter from '../lib/hooks/useFilter';
 
 import 'react-datepicker/dist/react-datepicker.css';
 
 const columns = [
-  { Header: 'Transaction ID', accessor: 'transaction_pk' },
-  { Header: 'Start Date', accessor: 'start_timestamp' },
-  { Header: 'Stop Date', accessor: 'stop_timestamp' },
-  { Header: 'Start Value', accessor: 'start_value' },
-  { Header: 'Stop Value', accessor: 'stop_value' },
+  { Header: 'Transaction ID', accessor: 'transactionId' },
+  { Header: 'Start Date', accessor: 'start' },
+  { Header: 'Stop Date', accessor: 'stop' },
+  { Header: 'Charging Time', accessor: 'chargingTime' },
+  { Header: 'Energy (Wh)', accessor: 'energy' },
+  { Header: 'Cost (Rp)', accessor: 'cost' },
 ] as const;
 
 const DailyPage: NextPage = () => {
   const [startDate, setStartDate] = useState<Date | null>(null);
-  const { data, error, loading } = useFilter<{ value: DailyRawData[] }>(
+  const {
+    data: rawData,
+    error,
+    loading,
+  } = useFilter<{ value: DailyRawData[] }>(
     `${process.env.NEXT_PUBLIC_DATA_PROVIDER}/daily`,
     startDate,
     dateRawFormat
   );
+
+  const data = useMemo(() => {
+    if (rawData) return parseDailyData(rawData.value);
+  }, [rawData]);
+
+  const displayData = useMemo(() => {
+    if (data) return displayDailyData(data);
+  }, [data]);
 
   return (
     <Layout title="Daily Transaction">
@@ -40,8 +58,8 @@ const DailyPage: NextPage = () => {
         <p>There is an error</p>
       ) : loading ? (
         <p>Loading...</p>
-      ) : data ? (
-        <Table columns={columns} data={data.value} />
+      ) : displayData ? (
+        <Table columns={columns} data={displayData} />
       ) : null}
     </Layout>
   );
